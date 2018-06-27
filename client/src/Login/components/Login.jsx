a6cc5df79a003f00630f45f0fb3804ff0bc77b62
Samuel Kim <samuel.kim@upstart.com>
2018-06-26 17:04:03 -0700

    add FE email and login components

---
+++ client/src/Login/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Login.css'
+
+class Login extends React.PureComponent {
+  render() {
+    return (
+      <div className="login">
+      </div>
+    )
+  }
+}
+
+export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,11 @@ import './index.css';
 
 //components
 import App from './App';
-import Clover from './Clover';
+import Login from './Login/components/Login';
 import Dash from './Dash/components/Dash';
+import Email from './Email/components/Email';
 import Feedback from './Feedback/components/Feedback';
+import Clover from './Clover';
 import NotFound from './NotFound';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -17,9 +19,11 @@ const appRouter = (
   <Router>
     <Switch>
       <Route exact path="/" component={App}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/dash" component={Dash}/>
+      <Route exact path="/email" component={Email}/>
+      <Route exact path="/feedback" component={Feedback}/>
       <Route exact path="/clover" component={Clover}/>
-      <Route path="/dash" component={Dash}/>
-      <Route path="/feedback" component={Feedback}/>
       <Route component={ NotFound } />
     </Switch>
   </Router>
